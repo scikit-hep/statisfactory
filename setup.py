1eb6e4346db6fd861217c9d8caec403da9fbf5a9
Giordon Stark <kratsg@gmail.com>
2019-07-12 11:03:30 -0700

    initial setup.py to squat pypi cream

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cream',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://giordonstark.com.com',
+    author='Giordon Stark',
+    author_email='gstark@cern.ch',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
